37759ae34b8ada4041325fa582ad5f145c6cdc4b
wntitus <wesn.titus@gmail.com>
2018-08-27 11:56:51 -0400

    adding jawsdb env var

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 //requiring the mysql package and setting up the database connection to be exported
 const mysql = require('mysql');
+var connection;
 
-const connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : 'root',
-    port : 3306,
-    database : 'burgers_db'
-})
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host : 'localhost',
+        user : 'root',
+        password : 'root',
+        port : 3306,
+        database : 'burgers_db'
+    })
+}
+
 
 module.exports = connection;
